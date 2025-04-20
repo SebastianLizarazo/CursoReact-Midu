ebe3272364ac4f10555294293f42a5e09db4c712
SebastianLizarazo <sdlizarazo9@misena.edu.co>
2025-04-20 15:11:48 -0500

    coments added to index.css

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,15 +1,43 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
+const users = [
+    {
+        userName: 'midudev',
+        name: 'Miguel Ángel Durán',
+        isFollowing: true
+    },
+    {
+        userName: 'pheralb',
+        name: 'Pablo Hernandez',
+        isFollowing: false
+    },
+    { 
+        userName: 'PacoHdezs',
+        name: 'Paco Hernández',
+        isFollowing: true
+    },
+    {
+        userName: 'jorge-hernandez',
+        name: 'Jorge Hernández',
+        isFollowing: false
+    }
+]
+
 export function App() {
     return(
         <section className="App">  
-            <TwitterFollowCard isFollowing userName="midudev">
-                Miguel Ángel Durán
-            </TwitterFollowCard>
-            <TwitterFollowCard isFollowing={false} userName="pheralb">
-                Pablo Hernandez
-            </TwitterFollowCard>
+            {
+                users.map(({userName, name, isFollowing}) =>(
+                    <TwitterFollowCard
+                        key={userName}
+                        userName={userName}
+                        initialIsFollowing={isFollowing}
+                    >   
+                        {name}
+                    </TwitterFollowCard>
+                ))
+            }
         </section>
     )
 }
